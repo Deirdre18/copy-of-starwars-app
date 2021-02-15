f821e5d34de1e1b846020577b752cb68afeaa371
Deirdre18 <deirdreweldon18@gmail.com>
2021-02-15 02:42:05 +0000

    deleted unwanted file, modified environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyBUaKWl1dX6EH_FYmuVEVNGCD7CsFrPQPw",
+    authDomain: "starwars-fdc00.firebaseapp.com",
+    projectId: "starwars-fdc00",
+    storageBucket: "starwars-fdc00.appspot.com",
+    messagingSenderId: "479048779088",
+    appId: "1:479048779088:web:62e6d5f6a14215b879d855",
+    measurementId: "G-PSGPRR6F0Q",
+    databaseURL: ""
   }
 };
